20a6fddb888438e75ab18c3afdf7276213eea7a4
AnnaTheCheeseWizard <zukanna857@gmail.com>
2025-12-04 02:55:52 +0200

    Inventory button

--- src/ui/hud.py
+++ src/ui/hud.py
@@ -1,10 +1,11 @@
 import pygame
 from typing import TYPE_CHECKING
 from src.entities.character import Character
+from src.ui.widgets import Button
+import src.config as cfg
 
 if TYPE_CHECKING:
     from src.app import App
-
 
 class HUD:
     def __init__(self, character: Character, app: "App"):
@@ -29,8 +30,30 @@ class HUD:
             self.life_icon = pygame.Surface((50, 50), pygame.SRCALPHA)
             pygame.draw.circle(self.life_icon, (200, 200, 200), (25, 25), 25)
 
+        button_width = 200
+        button_height = 50
+        button_x = 200
+        button_y = 240
+        
+        self.inv_button = Button(
+            pygame.Rect(button_x, button_y, button_width, button_height),
+            "INVENTORY",
+            (100, 100, 100),
+            (150, 150, 150),
+            self.font,
+            (255, 255, 255),
+            10,
+            on_click=self.toggle_inventory
+        )
+
+    def toggle_inventory(self):
+        self.app.INV_manager.player_inventory_opened = not self.app.INV_manager.player_inventory_opened
+
     def handle_event(self, event: pygame.event.Event):
-        pass
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.inv_button.rect.collidepoint(event.pos):
+                if self.inv_button.on_click:
+                    self.inv_button.on_click()
 
     def draw(self, screen: pygame.Surface):
         icon_x, icon_y = 200, 120
@@ -51,6 +74,7 @@ class HUD:
         # Draw Border
         pygame.draw.rect(screen, (255, 255, 255), (bar_x, bar_y, bar_width, bar_height), 3)
 
+
         lives_icon_x = icon_x
         lives_icon_y = icon_y + 60
 
@@ -58,3 +82,5 @@ class HUD:
 
         lives_text = self.font.render(f"x {self.character.death_count}", True, (255, 255, 255))
         screen.blit(lives_text, (lives_icon_x + 60, lives_icon_y + 5))
+
+        self.inv_button.draw(screen)
