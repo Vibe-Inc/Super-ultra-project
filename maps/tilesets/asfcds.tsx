79ee550fc5912d921516e3c94e4849da5b9a2526
Arsenii <holoborodko123@gmail.com>
2026-06-06 22:59:27 +0300

    fix forest->temple transition

---
+++ maps/tilesets/asfcds.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="asfcds" tilewidth="32" tileheight="32" tilecount="70" columns="10">
+ <image source="wall-8 - 2 tiles tall-transparency.png" width="320" height="224"/>
+</tileset>
