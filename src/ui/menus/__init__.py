a103e2b4ed56e5d0c6160ee5f2cc71f604d54472
RybakovDenys <rybakovdenis2007@gmail.com>
2026-06-06 18:42:42 +0300

    Fix

--- src/ui/menus/__init__.py
+++ src/ui/menus/__init__.py
@@ -5,3 +5,33 @@ Provides base menu class, main menu, settings, credits, pause, skillbar,
 skilltree, recipe book, wiki, collection book, arcane quests, mysterium
 magnum, save/load, and smeltery workstation menus.
 """
+
+from src.ui.menus.base import Menu
+from src.ui.menus.main_menu import MainMenu
+from src.ui.menus.settings_menu import SettingsMenu
+from src.ui.menus.skillbar_menu import SkillbarMenu
+from src.ui.menus.skilltree_menu import SkillTreeMenu
+from src.ui.menus.credits_menu import CreditsMenu
+from src.ui.menus.pause_menu import PauseMenu
+from src.ui.menus.save_load_menu import SaveLoadMenu
+from src.ui.menus.recipe_book import RecipeBookMenu
+from src.ui.menus.wiki_menu import WikiMenu
+from src.ui.menus.collection_book import CollectionBookMenu
+from src.ui.menus.arcane_quest_menu import ArcaneQuestMenu
+from src.ui.menus.mysterium_magnum import MysteriumMagnumMenu
+
+__all__ = [
+    "Menu",
+    "MainMenu",
+    "SettingsMenu",
+    "SkillbarMenu",
+    "SkillTreeMenu",
+    "CreditsMenu",
+    "PauseMenu",
+    "SaveLoadMenu",
+    "RecipeBookMenu",
+    "WikiMenu",
+    "CollectionBookMenu",
+    "ArcaneQuestMenu",
+    "MysteriumMagnumMenu",
+]
